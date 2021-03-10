c959c865dfb57aeb6ff2086a89f0d339eadb794d
Nmuta Jones <nmuta1@gmail.com>
2021-03-10 14:56:33 +0300

    Initial commit of voting app

---
+++ voting/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Voting from './Voting'
+
+
+function App() {
+  return (
+    <div className="App">
+       <Voting />
+    </div>
+  );
+}
+
+export default App;
---
+++ voting/src/Voting.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react';
+import './App.css';
+
+class Voting extends Component{
+	constructor(props){
+		super(props);
+		 
+	}
+
+	render(){
+		return(
+			<div> Welcome to the Voting app </div>
+		);
+	}
+}
+export default Voting;
